3ae0e50f735179dc5c43738030ddc9d434715939
Petr Jahoda <45359845+petrjahoda@users.noreply.github.com>
2020-12-23 08:27:43 +0100

    Update main.go

--- main.go
+++ main.go
@@ -7,6 +7,12 @@ import (
 	"time"
 )
 
+var (
+	serviceIsRunning bool
+	programIsRunning bool
+	writingSync      sync.Mutex
+)
+
 const serviceName = "Medium service"
 const serviceDescription = "Simple service, just for fun"
 
@@ -14,17 +20,26 @@ type program struct{}
 
 func (p program) Start(s service.Service) error {
 	fmt.Println(s.String() + " started")
+	writingSync.Lock()
+	serviceIsRunning = true
+	writingSync.Unlock()
 	go p.run()
 	return nil
 }
 
 func (p program) Stop(s service.Service) error {
+	writingSync.Lock()
+	serviceIsRunning = false
+	writingSync.Unlock()
+	for programIsRunning {
+		fmt.Println(s.String() + " stopping...")
+		time.Sleep(1 * time.Second)
+	}
 	fmt.Println(s.String() + " stopped")
 	return nil
 }
 
 func (p program) run() {
-
 	for serviceIsRunning {
 		writingSync.Lock()
 		programIsRunning = true
@@ -34,10 +49,6 @@ func (p program) run() {
 		writingSync.Lock()
 		programIsRunning = false
 		writingSync.Unlock()
-
-	for {
-		fmt.Println("Service is running")
-		time.Sleep(1 * time.Second)
 	}
 }
 
