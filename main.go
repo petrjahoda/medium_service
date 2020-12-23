6c3fab808a3c2368490ad692aa1f07a65ae34ec4
petrjahoda <petrjahoda@protonmail.com>
2020-12-23 08:14:01 +0100

    Chapter_1
    Took 2 minutes

--- main.go
+++ main.go
@@ -2,18 +2,8 @@ package main
 
 import (
 	"fmt"
-	"github.com/julienschmidt/httprouter"
 	"github.com/kardianos/service"
-	"net/http"
-	"os"
-	"sync"
 	"time"
-)
-
-var (
-	serviceIsRunning bool
-	programIsRunning bool
-	writingSync      sync.Mutex
 )
 
 const serviceName = "Medium service"
@@ -23,36 +13,19 @@ type program struct{}
 
 func (p program) Start(s service.Service) error {
 	fmt.Println(s.String() + " started")
-	writingSync.Lock()
-	serviceIsRunning = true
-	writingSync.Unlock()
 	go p.run()
 	return nil
 }
 
 func (p program) Stop(s service.Service) error {
-	writingSync.Lock()
-	serviceIsRunning = false
-	writingSync.Unlock()
-	for programIsRunning {
-		fmt.Println(s.String() + " stopping...")
-		time.Sleep(1 * time.Second)
-	}
 	fmt.Println(s.String() + " stopped")
 	return nil
 }
 
 func (p program) run() {
-	router := httprouter.New()
-	router.ServeFiles("/js/*filepath", http.Dir("js"))
-	router.ServeFiles("/css/*filepath", http.Dir("css"))
-
-	router.GET("/", serveHomepage)
-
-	err := http.ListenAndServe(":82", router)
-	if err != nil {
-		fmt.Println("Problem starting web server: " + err.Error())
-		os.Exit(-1)
+	for {
+		fmt.Println("Service is running")
+		time.Sleep(1 * time.Second)
 	}
 }
 
